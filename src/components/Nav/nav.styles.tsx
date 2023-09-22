a5ba128f2da304066f3ad458aebb27ce3d91c3a1
Omar Fahmy <Ofahmy1234@gmail.com>
2023-09-22 19:28:34 +0300

    feat: responsiveness without navbar

--- src/components/Nav/nav.styles.tsx
+++ src/components/Nav/nav.styles.tsx
@@ -1,5 +1,23 @@
 import styled from "styled-components";
 
+export const sizes = {
+    xsmall: "320px",
+    small: "480px",
+    medium: "768px",
+    large: "1024px",
+    xlarge: "1200px",
+    xxlarge: "1440px",
+};
+
+export const devices = {
+    xsmall: `(max-width: ${sizes.xsmall})`,
+    small: `(min-width: ${sizes.xsmall}) and (max-width: ${sizes.small})`,
+    medium: `(min-width: ${sizes.small}) and (max-width: ${sizes.medium})`,
+    large: `(min-width: ${sizes.medium}) and (max-width: ${sizes.large})`,
+    xlarge: `(min-width: ${sizes.large}) and (max-width: ${sizes.xlarge})`,
+    xxlarge: `(min-width: ${sizes.xlarge}) and (max-width: ${sizes.xxlarge})`,
+};
+
 export const Navbar = styled.div`
     position: absolute;
     top: 0;
@@ -30,28 +48,54 @@ export const LinkGroup = styled.div`
         font-size: 1.1vw;
         color: white;
         opacity: 0.7;
+        @media ${devices.xlarge} {
+            font-size: 1em;
+        }
+        @media ${devices.large} {
+            font-size: 0.8em;
+        }
     }
 `;
 
 export const OraganizationLogoImg = styled.img`
     z-index: 20;
     object-fit: contain;
+    padding: 0;
+    margin-right: -10px;
+    @media ${devices.small} {
+        margin-right: -15px;
+        transform: scale(0.8);
+    }
     /* width: 100px; */
 `;
 export const OrganizationGroup = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-left: 1vw;
+    margin-left: -5px;
+
+    /* gap: -10px; */
 `;
 
 export const OrganizationLogo = styled.h3`
-    font-family: Lato;
+    /* font-family: Lato; */
     color: white;
+    opacity: 0.9;
+
     /* margin-left: 2vw; */
     font-family: Lato;
     font-weight: lighter;
-    font-size:1.5em;
+    font-size: 1.5em;
+    padding: 0;
+    margin: 0;
+
+    @media ${devices.large} {
+        font-size: 0.9em;
+        /* transform: scale(0.8) */
+    }
+    @media ${devices.small} {
+        font-size: 0.9em;
+    }
     /* font-size: 1.5vw; */
     /* margin-top: 10px; */
 `;
