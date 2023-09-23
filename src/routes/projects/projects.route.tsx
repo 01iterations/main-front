34eca58a6e90772a9be42de9586ad61acda0e9c3
Omar Fahmy <Ofahmy1234@gmail.com>
2023-09-23 15:53:55 +0300

    feat: add slides for each view

--- src/routes/projects/projects.route.tsx
+++ src/routes/projects/projects.route.tsx
@@ -4,7 +4,8 @@ import {
     StyledCarousel,
 } from "./projects.style";
 import Carousel from "react-bootstrap/Carousel";
-import slideData from "./projects.json";
+import slideDataMobile from "./projects-mobile.json";
+import slideDataDesktop from "./projects-desktop.json";
 import { useEffect, useState } from "react";
 import { projectSlides } from "../../types/projects";
 import Nav from "../../components/Nav/nav.component";
@@ -12,9 +13,34 @@ import { ProjectCard } from "../../components/ProjectCard/projectCard.component"
 
 export function Projects() {
     const [slides, setSlides] = useState<projectSlides[]>([]);
+    const [screenWidth, setScreenWidth] = useState<number>(window.innerWidth);
+    const breakpoint = 768; // breakpoint for mobile/desktop view
+
     useEffect(() => {
-        setSlides(slideData);
+        // function to update screen width
+        const handleResize = () => {
+            setScreenWidth(window.innerWidth);
+        };
+        // add window resize event listener on mount
+        window.addEventListener("resize", handleResize);
+        // remove window resize event listener on unmount
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
     }, []);
+
+    useEffect(() => {
+        // conditional rendering of slide data based on screen width
+        console.warn({ breakpoint, screenWidth });
+
+        if (screenWidth < breakpoint) {
+            // mobile view
+            setSlides(slideDataMobile);
+        } else {
+            // desktop view
+            setSlides(slideDataDesktop);
+        }
+    }, [screenWidth]);
     return (
         <>
             <Nav />
@@ -32,7 +58,7 @@ export function Projects() {
                     ))}
                 </StyledCarousel>
                 <ProjectsGrid>
-                    {slideData.map((slide: projectSlides, index: number) => (
+                    {slides.map((slide: projectSlides, index: number) => (
                         <ProjectCard
                             key={index}
                             courseTitle={slide.quote.title}
