5cc22ce116b91771b410ce0eb2f3be68e746b82b
Omar Fahmy <ofahmy1234@gmail.com>
2023-11-23 11:14:15 +0200

    fix: loading screen

--- src/components/LoadingData/LoadingData.styles.tsx
+++ src/components/LoadingData/LoadingData.styles.tsx
@@ -7,6 +7,8 @@ export type LoadingDataProps = {
 
 export const LoadingContainer = styled.div<LoadingDataProps>`
   overflow: hidden;
+  width: 100%;
+  height: 100vh;
   opacity: ${(props) => (props.isLoading ? 1 : 0)};
   pointer-events: ${(props) => (props.isLoading ? "auto" : "none")};
   z-index:1000;
