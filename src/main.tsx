9456f10ebc181fd2ffbc88949d51636bc055a3fd
Omar Fahmy <ofahmy1234@gmail.com>
2023-12-01 15:11:58 +0200

    feat: about route in router

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-    createHashRouter,
-    RouterProvider,
-} from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/home/home.route";
 import Projects from "./routes/projects/projects.route";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import CourseDetails from "./routes/course-details/courseDetails.route";
 import CoursesView from "./routes/courses-view/coursesView.route";
+import { About } from "./routes/about/about.route";
 
 const router = createHashRouter([
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/projects",
-        element: <Projects />,
-    },
-    {
-        path: "/courses/:courseNum",
-        element: <CourseDetails />,
-    },{
-        path:"/courses",
-        element: <CoursesView />
-        }
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+  },
+  {
+    path: "/courses/:courseNum",
+    element: <CourseDetails />,
+  },
+  {
+    path: "/courses",
+    element: <CoursesView />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
